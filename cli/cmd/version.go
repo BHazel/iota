188b2f8da68e45806b0e5f4f8a1c0f37a138d704
Benedict W. Hazel <bwhazel@gmail.com>
2018-01-27 17:11:39 +0000

    Add version command.

---
+++ cli/cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/bhazel/iota/config"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use: "version",
+	Short: "Prints the Iota version.",
+	Long: "Prints the Iota version to standard output.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Iota v%s\n", config.CurrentVersion)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
---
+++ config/version.go
@@ -0,0 +1,5 @@
+package config
+
+const (
+	CurrentVersion = "0.0.1"
+)
