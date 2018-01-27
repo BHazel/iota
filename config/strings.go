6d58c2408f28b630b2dd8656abfa6fab6a3d058c
Benedict W. Hazel <bwhazel@gmail.com>
2018-01-27 18:35:37 +0000

    Change string constants to not use literals.

--- config/strings.go
+++ config/strings.go
@@ -1,9 +1,9 @@
 package config
 
 const (
-	ChecklistFileExtension = `checklist`
-	CurrentVersion = `0.0.1`
-	GenericError = `ERROR: %s`
-	InitErrorFileExists = `ERROR: File "%s" already exists.`
-	InitFileCreated = `New checklist created in "%s".`
+	ChecklistFileExtension = "checklist"
+	CurrentVersion = "0.0.1"
+	GenericError = "ERROR: %s\n"
+	InitErrorFileExists = "ERROR: File \"%s\" already exists.\n"
+	InitFileCreated = "New checklist created in \"%s\".\n"
 )
