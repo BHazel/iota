04441b7de47a6cc958f44016dcd249906a1f2d85
Benedict W. Hazel <bwhazel@gmail.com>
2018-01-27 17:11:25 +0000

    Add Cobra package and root command.

---
+++ cli/cmd/root.go
@@ -0,0 +1,15 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "iota",
+	Short: "Manages checklists.",
+	Long: "Manages plain-text checklists conforming to the Iota checklist specification.",
+}
+
+func Execute() {
+	rootCmd.Execute()
+}
---
+++ cli/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/bhazel/iota/cli/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
